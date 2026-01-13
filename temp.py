4ddfc3189ad799580c03d6e91f141a6093e9fee9
hahohihiho <lsuuny513@gmail.com>
2026-01-13 15:42:37 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="My First Streanlit")
+
+st.title("My First Streamlit")
+st.write("Tis app is bult with Streamlit!")
+st.balloons()
